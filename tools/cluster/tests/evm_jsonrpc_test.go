99496ea63dfaa8fd55c92585cf3058a829f51752
Diego Essaya <dessaya@gmail.com>
2023-04-06 11:55:26 -0300

    add jsonrpc spam cluster test

--- tools/cluster/tests/evm_jsonrpc_test.go
+++ tools/cluster/tests/evm_jsonrpc_test.go
@@ -6,13 +6,17 @@ package tests
 import (
 	"context"
 	"crypto/ecdsa"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"math/big"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -151,4 +155,143 @@ func TestEVMJsonRPCClusterAccessNode(t *testing.T) {
 	env := newChainEnv(t, clu, chain)
 	e := newClusterTestEnv(t, env, 4) // node #4 is an access node
 	e.TestRPCGetLogs()
+}
+
+func TestEVMJsonRPCSpam(t *testing.T) {
+	run := createTestWrapper(t, 4, []int{0, 1, 2, 3})
+	run(t, func(t *testing.T, chenv *ChainEnv) {
+		e := newClusterTestEnv(t, chenv, 0)
+
+		walletKey, walletAddr, err := e.Clu.NewKeyPairWithFunds()
+		require.NoError(t, err)
+
+		totalL2 := e.Clu.L1BaseTokens(walletAddr) / 2
+		const nAccounts = 10
+		l2PerEthAccount := totalL2 / (uint64(nAccounts) * 2)
+
+		{
+			tx, err := e.Chain.Client(walletKey).DepositFunds(totalL2)
+			_, err = e.Chain.AllNodesMultiClient().WaitUntilAllRequestsProcessedSuccessfully(e.Chain.ChainID, tx, 30*time.Second)
+			require.NoError(t, err)
+		}
+
+		var ethKeys []*ecdsa.PrivateKey
+		var ethAddrs []common.Address
+		{
+			for i := 0; i < nAccounts; i++ {
+				ethKey, ethAddr := newEthereumAccount()
+				req, err := e.Chain.Client(walletKey).PostOffLedgerRequest(context.Background(), accounts.Contract.Hname(), accounts.FuncTransferAllowanceTo.Hname(), chainclient.PostRequestParams{
+					Args: map[kv.Key][]byte{
+						accounts.ParamAgentID: codec.EncodeAgentID(isc.NewEthereumAddressAgentID(ethAddr)),
+					},
+					Allowance: isc.NewAssetsBaseTokens(l2PerEthAccount),
+				})
+				require.NoError(t, err)
+				_, err = e.Chain.AllNodesMultiClient().WaitUntilRequestProcessedSuccessfully(e.Chain.ChainID, req.ID(), 30*time.Second)
+				require.NoError(t, err)
+				ethKeys = append(ethKeys, ethKey)
+				ethAddrs = append(ethAddrs, ethAddr)
+			}
+		}
+
+		done := make(chan bool)
+		wsdone := make(chan bool)
+
+		type wsblock struct {
+			header *types.Header
+			ts     time.Time
+		}
+
+		var wsblocks []wsblock
+		go func() {
+			defer close(wsdone)
+			ch := make(chan *types.Header, 10)
+
+			sub, err := e.DialWebsocket().SubscribeNewHead(context.Background(), ch)
+			require.NoError(t, err)
+			defer sub.Unsubscribe()
+
+			for {
+				select {
+				case header := <-ch:
+					wsblocks = append(wsblocks, wsblock{
+						header: header,
+						ts:     time.Now(),
+					})
+
+				case err := <-sub.Err():
+					require.NoError(t, err)
+
+				case <-done:
+					return
+				}
+			}
+		}()
+
+		t.Log("=========== starting the spam ==============")
+
+		endTime := time.Now().Add(60 * time.Second)
+
+		{
+			var wg sync.WaitGroup
+			wg.Add(nAccounts)
+			for i := 0; i < nAccounts; i++ {
+				go func(i int, ethKey *ecdsa.PrivateKey, ethAddr common.Address) {
+					defer wg.Done()
+					nonce := e.NonceAt(ethAddr)
+					for time.Now().Before(endTime) {
+						toAddress := common.Address{}
+						gasLimit := uint64(100_000)
+						value := big.NewInt(100)
+						tx, err := types.SignTx(
+							types.NewTransaction(nonce, toAddress, value, gasLimit, evm.GasPrice, nil),
+							e.Signer(),
+							ethKey,
+						)
+						require.NoError(t, err)
+						err = e.Client.SendTransaction(context.Background(), tx)
+						require.NoError(t, err)
+						err = e.WaitTxConfirmed(tx.Hash())
+						require.NoError(t, err)
+						nonce++
+					}
+				}(i, ethKeys[i], ethAddrs[i])
+			}
+			wg.Wait()
+		}
+
+		close(done)
+		<-wsdone
+
+		type blockdata struct {
+			Index              uint64
+			Received           string
+			BlockinfoTimestamp string
+			TotalReqs          uint32
+		}
+		var blocks []blockdata
+		var outOfOrder []uint64
+		for i, b := range wsblocks {
+			index := b.header.Number.Uint64()
+			if i > 0 && index != wsblocks[i-1].header.Number.Uint64()+1 {
+				outOfOrder = append(outOfOrder, index)
+			}
+
+			block, _, err := e.Clu.
+				WaspClient().CorecontractsApi.BlocklogGetBlockInfo(context.Background(), e.Chain.ChainID.String(), uint32(index)).
+				Execute() //nolint:bodyclose // false positive
+			require.NoError(t, err)
+
+			blocks = append(blocks, blockdata{
+				Index:              index,
+				Received:           b.ts.Format("15:04:05.00"),
+				BlockinfoTimestamp: block.Timestamp.Format("15:04:05.00"),
+				TotalReqs:          block.TotalRequests,
+			})
+		}
+		t.Logf("total %d blocks", len(blocks))
+		j, _ := json.Marshal(blocks)
+		t.Logf("blocks: %s", j)
+		require.Empty(t, outOfOrder)
+	})
 }
