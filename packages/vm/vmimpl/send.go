d45e40fcbc9c45b754c47aabc0ab97364e6566d5
Diego Essaya <dessaya@gmail.com>
2025-05-21 14:30:57 -0300

    fix: common account balance after send

--- packages/vm/vmimpl/send.go
+++ packages/vm/vmimpl/send.go
@@ -4,9 +4,13 @@ import (
 	"github.com/iotaledger/wasp/packages/isc"
 )
 
-const MaxPostedOutputsInOneRequest = 4
-
 func (reqctx *requestContext) send(params isc.RequestParameters) {
 	// simply send assets to a L1 address
 	reqctx.vm.txbuilder.SendAssets(params.TargetAddress.AsIotaAddress(), params.Assets)
+
+	account := reqctx.CurrentContractAccountID()
+	reqctx.accountsStateWriter(false).DebitFromAccount(account, params.Assets.Coins)
+	for obj := range params.Assets.Objects.Iterate() {
+		reqctx.accountsStateWriter(false).DebitObjectFromAccount(account, obj.ID)
+	}
 }
